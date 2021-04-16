d81fe89e9aeefbf5152e5074d691f9f208867974
ernest1027 <36519873+ernest1027@users.noreply.github.com>
2021-04-15 21:40:08 -0400

    Update CapstoneRealese.java

--- TeamCode/src/main/java/com/team16488/library/subsystems/CapstoneRealese.java
+++ TeamCode/src/main/java/com/team16488/library/subsystems/CapstoneRealese.java
@@ -3,25 +3,25 @@ package com.team16488.library.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class CapstoneRealese extends Subsystem{
-    public Servo realseCap;
+public class CapstoneRelease extends Subsystem{
+    public Servo releaseCap;
 
-    boolean realse;
+    boolean release;
 
-    public CapstoneRealese(HardwareMap map){
-        realseCap = map.servo.get("CapRelease");
+    public CapstoneRelease(HardwareMap map){
+        releaseCap = map.servo.get("CapRelease");
     }
 
     @Override
     public void update() {
-        if(realse){
-            realseCap.setPosition(0);
+        if(release){
+            releaseCap.setPosition(0);
         }
-        if(!realse){
-            realseCap.setPosition(1.0);
+        if(!release){
+            releaseCap.setPosition(1.0);
         }
     }
-    public void setRealse(boolean realse){
-        this.realse = realse;
+    public void setRelease(boolean release){
+        this.release = release;
     }
 }
